84ea32b3f99c92f98646f8e6131ac964cc4cea68
owosrl <polina@hypercute.eu>
2023-03-25 21:31:02 +0100

    fixed dependencies, added testes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -9,6 +9,36 @@ def test_index():
     response = client.get('/')
     assert response.status_code == 200
 
+def test_signup():
+    app = create_app('lanotte24ore.test')
+    client = app.test_client()
+    response = client.get('/signup')
+    assert response.status_code == 200
+
+def test_categories():
+    app = create_app('lanotte24ore.test')
+    @app.template_filter()
+    def format_datetime(value):
+        return datetime.strptime(value, "%Y-%m-%dT%H:%M:%SZ").strftime("%d %B %Y")
+    client = app.test_client()
+    response = client.get('/categories?category=general')
+    assert response.status_code == 200
+
+def test_search():
+    app = create_app('lanotte24ore.test')
+    @app.template_filter()
+    def format_datetime(value):
+        return datetime.strptime(value, "%Y-%m-%dT%H:%M:%SZ").strftime("%d %B %Y")
+    client = app.test_client()
+    response = client.get('/search?search=Giulio+Andreotti')
+    assert response.status_code == 200
+
+def test_about():
+    app = create_app('lanotte24ore.test')
+    client = app.test_client()
+    response = client.get('/about')
+    assert response.status_code == 200
+
 def test_login_get():
     app = create_app('lanotte24ore.test')
     client = app.test_client()
